87a5a14d84f7d403e2a6a11689f09717b2db1c8b
ApcGamer <abhisingh3@hotmail.com>
2021-02-20 22:45:10 +0530

    flexbox project in react

---
+++ build/precache-manifest.2df86b21b2ab0e714050bec0305c7158.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "106911ba8466b1d98380",
+    "url": "/trillio-flexbox-react/static/css/main.f4ca7d95.chunk.css"
+  },
+  {
+    "revision": "106911ba8466b1d98380",
+    "url": "/trillio-flexbox-react/static/js/main.7756a329.chunk.js"
+  },
+  {
+    "revision": "f1a3e531394a299a3ae6",
+    "url": "/trillio-flexbox-react/static/js/runtime~main.bc64ba90.js"
+  },
+  {
+    "revision": "bf6590d8901ea6683836",
+    "url": "/trillio-flexbox-react/static/js/2.a360a728.chunk.js"
+  },
+  {
+    "revision": "16f15536a0a7f7f93995",
+    "url": "/trillio-flexbox-react/static/js/3.2d1d518a.chunk.js"
+  },
+  {
+    "revision": "f20a50bb79803cf7dea48d545efd7834",
+    "url": "/trillio-flexbox-react/static/media/bookmark.f20a50bb.svg"
+  },
+  {
+    "revision": "7e246b49875e3f5af2a3233f8dcbf822",
+    "url": "/trillio-flexbox-react/static/media/chat.7e246b49.svg"
+  },
+  {
+    "revision": "263eb0caf45ea17c0b1c1eed1f067d05",
+    "url": "/trillio-flexbox-react/static/media/user.263eb0ca.jpg"
+  },
+  {
+    "revision": "5a550a3da0d63bfa7ec73ac1f1acdbba",
+    "url": "/trillio-flexbox-react/static/media/hotel-1.5a550a3d.jpg"
+  },
+  {
+    "revision": "5468a3b162abcbb501141ce0e7f4aa6e",
+    "url": "/trillio-flexbox-react/static/media/hotel-2.5468a3b1.jpg"
+  },
+  {
+    "revision": "7af5efbc95e0464867f4327aee208ad5",
+    "url": "/trillio-flexbox-react/static/media/hotel-3.7af5efbc.jpg"
+  },
+  {
+    "revision": "c7fedfa061fe1cdacbbfc58ca273898f",
+    "url": "/trillio-flexbox-react/static/media/chevron-thin-right.c7fedfa0.svg"
+  },
+  {
+    "revision": "f9737d3ed2e6a8b07c15ff443e9d173d",
+    "url": "/trillio-flexbox-react/static/media/magnifying-glass.f9737d3e.svg"
+  },
+  {
+    "revision": "7d0871a2719dfaa89abcdaf2a6d50c74",
+    "url": "/trillio-flexbox-react/static/media/star.7d0871a2.svg"
+  },
+  {
+    "revision": "a68266a33ff4a9b2129e46d0184fa038",
+    "url": "/trillio-flexbox-react/static/media/location-pin.a68266a3.svg"
+  },
+  {
+    "revision": "c5057c546f5632222015fcd1f9870fa6",
+    "url": "/trillio-flexbox-react/static/media/home.c5057c54.svg"
+  },
+  {
+    "revision": "1c7c557e9714f393a4c7925e4a1c9840",
+    "url": "/trillio-flexbox-react/static/media/aircraft-take-off.1c7c557e.svg"
+  },
+  {
+    "revision": "d7d0212d8bc3c46656875534372e6e95",
+    "url": "/trillio-flexbox-react/static/media/key.d7d0212d.svg"
+  },
+  {
+    "revision": "df9b239dada5a12fc53049b2e6b040ae",
+    "url": "/trillio-flexbox-react/static/media/map.df9b239d.svg"
+  },
+  {
+    "revision": "f17b7d3e60a6968515632ea1671d8132",
+    "url": "/trillio-flexbox-react/index.html"
+  }
+];
