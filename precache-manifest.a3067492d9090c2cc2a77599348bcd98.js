5c6f3f3344fdce8613ddb898cf796ef213a0219b
ApcGamer <abhisingh3@hotmail.com>
2021-02-21 20:05:13 +0530

    Updates

---
+++ precache-manifest.a3067492d9090c2cc2a77599348bcd98.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "df965c7f8bc8b1d73f2c",
+    "url": "/trillio-hotels/static/css/main.fb9db964.chunk.css"
+  },
+  {
+    "revision": "df965c7f8bc8b1d73f2c",
+    "url": "/trillio-hotels/static/js/main.7756a329.chunk.js"
+  },
+  {
+    "revision": "4cba451eb427f10ee2f3",
+    "url": "/trillio-hotels/static/js/runtime~main.4e6e2d5f.js"
+  },
+  {
+    "revision": "bf6590d8901ea6683836",
+    "url": "/trillio-hotels/static/js/2.a360a728.chunk.js"
+  },
+  {
+    "revision": "16f15536a0a7f7f93995",
+    "url": "/trillio-hotels/static/js/3.2d1d518a.chunk.js"
+  },
+  {
+    "revision": "f20a50bb79803cf7dea48d545efd7834",
+    "url": "/trillio-hotels/static/media/bookmark.f20a50bb.svg"
+  },
+  {
+    "revision": "7e246b49875e3f5af2a3233f8dcbf822",
+    "url": "/trillio-hotels/static/media/chat.7e246b49.svg"
+  },
+  {
+    "revision": "263eb0caf45ea17c0b1c1eed1f067d05",
+    "url": "/trillio-hotels/static/media/user.263eb0ca.jpg"
+  },
+  {
+    "revision": "5a550a3da0d63bfa7ec73ac1f1acdbba",
+    "url": "/trillio-hotels/static/media/hotel-1.5a550a3d.jpg"
+  },
+  {
+    "revision": "5468a3b162abcbb501141ce0e7f4aa6e",
+    "url": "/trillio-hotels/static/media/hotel-2.5468a3b1.jpg"
+  },
+  {
+    "revision": "7af5efbc95e0464867f4327aee208ad5",
+    "url": "/trillio-hotels/static/media/hotel-3.7af5efbc.jpg"
+  },
+  {
+    "revision": "c7fedfa061fe1cdacbbfc58ca273898f",
+    "url": "/trillio-hotels/static/media/chevron-thin-right.c7fedfa0.svg"
+  },
+  {
+    "revision": "f9737d3ed2e6a8b07c15ff443e9d173d",
+    "url": "/trillio-hotels/static/media/magnifying-glass.f9737d3e.svg"
+  },
+  {
+    "revision": "7d0871a2719dfaa89abcdaf2a6d50c74",
+    "url": "/trillio-hotels/static/media/star.7d0871a2.svg"
+  },
+  {
+    "revision": "a68266a33ff4a9b2129e46d0184fa038",
+    "url": "/trillio-hotels/static/media/location-pin.a68266a3.svg"
+  },
+  {
+    "revision": "c5057c546f5632222015fcd1f9870fa6",
+    "url": "/trillio-hotels/static/media/home.c5057c54.svg"
+  },
+  {
+    "revision": "1c7c557e9714f393a4c7925e4a1c9840",
+    "url": "/trillio-hotels/static/media/aircraft-take-off.1c7c557e.svg"
+  },
+  {
+    "revision": "d7d0212d8bc3c46656875534372e6e95",
+    "url": "/trillio-hotels/static/media/key.d7d0212d.svg"
+  },
+  {
+    "revision": "df9b239dada5a12fc53049b2e6b040ae",
+    "url": "/trillio-hotels/static/media/map.df9b239d.svg"
+  },
+  {
+    "revision": "2e5a94e866b23da61e67b57d67815e35",
+    "url": "/trillio-hotels/index.html"
+  }
+];
